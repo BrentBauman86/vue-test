fd76c6192f157a2c26591a42a366465c3a204fed
BrentBauman86 <brentbauman86@gmail.com>
2019-01-30 12:22:53 -0800

    install firebase and create login route/view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Skills from './components/Skills.vue';
 import About from './components/About.vue';
+import Login from './components/Login.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
         path: '/about/:name',
         name: 'about',
         component: About
-        }, 
+        },
+        { 
+        path: '/login',
+        name: 'login',
+        component: Login 
+        }
     ]
 })
