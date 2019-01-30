a1a964e10decf50b061a3b7f512b05b86cefd0ae
BrentBauman86 <brentbauman86@gmail.com>
2019-01-30 13:56:40 -0800

    enable firebase and allow user sign-up

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,20 +1,36 @@
 <template>
 <div class="sign-up">
     <h3>Create an account!</h3>
-    <input type="text" placeholder="Email"><br>
-    <input type="password" placeholder="Password"><br>
-    <button>Sign Up</button>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click='signUp'>Sign Up</button>
     <span>or go back to <router-link to='/login'>login</router-link>.</span>
     </div>
     </template>
 
     <script>
+    import firebase from 'firebase';
+
     export default {
-        name: 'signup',
+        name: 'signUp',
         data() {
-            return {};
+            return {
+                email: '',
+                password: ''
+            }
         },
-        methods: {}
+        methods: {
+            signUp: function() {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+                    function (user) {
+                        alert('Your accout has been created!')
+                    },
+                    function (err) {
+                        alert('Oops. ' + err.message)
+                    }
+                );
+            }
+        }
     }
     </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VeeValidate from 'vee-validate';
+import firebase from 'firebase';
 import router from './router';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 
 
 Vue.use(VeeValidate);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+  const config = {
+    apiKey: "AIzaSyDON7iKIBMp8Oh2ABLRjT4epc6ggaNMQB4",
+    authDomain: "vue-firebase-aa748.firebaseapp.com",
+    databaseURL: "https://vue-firebase-aa748.firebaseio.com",
+    projectId: "vue-firebase-aa748",
+    storageBucket: "vue-firebase-aa748.appspot.com",
+    messagingSenderId: "893822455968"
+  };
+
+  firebase.initializeApp(config);
 
 new Vue({
   router, 
