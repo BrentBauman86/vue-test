e94a9b7e4dc7120b11e91c89ef15bfc856703241
BrentBauman86 <brentbauman86@gmail.com>
2019-01-30 06:39:45 -0800

    import/install VueRouter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css';
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import VeeValidate from 'vee-validate';
 
+
+Vue.use(VueRouter)
 Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
